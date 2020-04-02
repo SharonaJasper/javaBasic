f606cf6886d1693ca2c6b515cd952a3d58415f95
SharonaJasper <sharonajasper@hotmail.com>
2020-04-02 16:58:24 +0200

    opdracht21 add

---
+++ src/Opdracht21.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Opdracht21 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your integer: ");
+        int number = scanner.nextInt();
+        System.out.println("The sum is " + getSum(number));
+    }
+
+    static int getSum(int n) {
+        int sum = 0;
+        /* Single line that calculates sum */
+        while (n > 0) {
+            sum += n % 10;
+            n /= 10;
+            System.out.println(n);
+        }
+            return sum;
+    }
+
+}
