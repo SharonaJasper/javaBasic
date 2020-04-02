ac6ec7e758185d135e98366e4f302116ad540211
SharonaJasper <sharonajasper@hotmail.com>
2020-04-02 13:15:08 +0200

    opdr5 add

---
+++ src/opdracht5.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class opdracht5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("value of x: ");
+        int x = scanner.nextInt();
+        System.out.println("value of y: ");
+        int y = scanner.nextInt();
+
+        if (x / y == 5) {
+            x = 100;
+        }
+        if (x * y == 5) {
+            x = 1;
+        }
+        if (x < y) {
+            x = x * 2;
+        }
+        if (x > y) {
+            x++;
+        }
+
+        System.out.println(x);
+        System.out.println(y);
+
+    }
+}
