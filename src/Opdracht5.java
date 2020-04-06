18789bbb2fe239506caadfe601e4b0225e71afde
SharonaJasper <sharonajasper@hotmail.com>
2020-04-06 15:52:09 +0200

    opdr5 add

--- src/Opdracht5.java
+++ src/Opdracht5.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class opdracht5 {
+public class Opdracht5 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("value of x: ");
