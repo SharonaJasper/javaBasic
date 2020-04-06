54e5b5220f1eb78f16b17871fcd314ae0a073b2c
SharonaJasper <sharonajasper@hotmail.com>
2020-04-06 15:49:46 +0200

    opdr1 add

--- src/Opdracht1.java
+++ src/Opdracht1.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class opdracht1 {
+public class Opdracht1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter article: ");
