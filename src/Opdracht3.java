8956c3a110c05c5746d4ba0edc480b6de118618f
SharonaJasper <sharonajasper@hotmail.com>
2020-04-06 15:51:36 +0200

    opdr3 add

--- src/Opdracht3.java
+++ src/Opdracht3.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class opdracht3 {
+public class Opdracht3 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter first side of the triangle: ");
