b96d40ac6fbc699ff5058fe529f19c00f20ad270
SharonaJasper <sharonajasper@hotmail.com>
2020-04-08 12:44:08 +0200

    TekenProgramma

--- src/ooDraw/Teken.java
+++ src/ooDraw/Teken.java
@@ -2,15 +2,15 @@ package ooDraw;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
+import java.awt.event.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Teken extends JFrame implements MouseListener {
+public class Teken extends JFrame implements MouseListener, ActionListener {
 
-    Figuur figuur = new Ovaal();
+    Figuur figuur = new Lijn();
     List<Figuur> figuren = new ArrayList<>();
+    JButton button = new JButton("Save to file");
 
 
     public static void main(String[] args) {
@@ -21,8 +21,62 @@ public class Teken extends JFrame implements MouseListener {
     }
 
     public Teken() {
+
+        //JMenuBar
+        JMenuBar menuBar;
+        JMenu menu;
+        JMenuItem JMenuItem;
+
+        //Create the menu bar.
+        menuBar = new JMenuBar();
+
+        //Build the first menu.
+        menu = new JMenu("Figure");
+        menu.setMnemonic(KeyEvent.VK_A);
+        menu.getAccessibleContext().setAccessibleDescription(
+                "The only menu in this program that has menu items");
+        menuBar.add(menu);
+
+        //a group of JMenuItems
+        JMenuItem rechthoek = new JMenuItem("Rechthoek");
+        menu.add(rechthoek);
+        //rechthoek.setActionCommand("rechthoek");
+        rechthoek.addActionListener(this);
+
+        JMenuItem ovaal = new JMenuItem("Ovaal");
+        menu.add(ovaal);
+        //ovaal.setActionCommand("ovaal");
+        ovaal.addActionListener(this);
+
+        JMenuItem lijn = new JMenuItem("Lijn");
+        menu.add(lijn);
+        //figuur = new Lijn();
+        //lijn.setActionCommand("lijn");
+        lijn.addActionListener(this);
+
+        JMenuItem driehoek = new JMenuItem("Driehoek");
+        menu.add(driehoek);
+        //driehoek.setActionCommand("driehoek");
+        driehoek.addActionListener(this);
+
+
+        //Build second menu in the menu bar.
+        menu = new JMenu("Color");
+        menu.setMnemonic(KeyEvent.VK_N);
+        menu.getAccessibleContext().setAccessibleDescription(
+                "This menu does nothing");
+        menuBar.add(menu);
+
+
+        this.setJMenuBar(menuBar);
+
+
+        getContentPane().add(button, BorderLayout.SOUTH);
+        button.addActionListener(this);
         addMouseListener(this);
     }
+
+
 
     @Override
     public void paint(Graphics g) {
@@ -35,11 +89,11 @@ public class Teken extends JFrame implements MouseListener {
     @Override
     public void mousePressed(MouseEvent e) {
         System.out.println("pressed");
-        if (e.getButton() == MouseEvent.BUTTON3) {
-            figuur = new Lijn();
-        } else {
-            figuur = new Ovaal();
-        }
+//        if (e.getButton() == MouseEvent.BUTTON3) {
+//            figuur = new Lijn();
+//        } else {
+//            figuur = new Ovaal();
+//        }
         figuur.x1 = e.getX();
         figuur.y1 = e.getY();
     }
@@ -52,6 +106,23 @@ public class Teken extends JFrame implements MouseListener {
 //        figuur = new Driehoek();
         repaint();
         figuren.add(figuur);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String command = e.getActionCommand();
+        System.out.println("actionperf");
+        if (command.equals("Rechthoek")) {
+            figuur = new Rechthoek();
+        } if (command.equals("Ovaal")) {
+            figuur = new Ovaal();
+        } if (command.equals("Lijn")) {
+            figuur = new Lijn();
+        } else {
+            figuur = new Driehoek();
+        }
+
+
     }
 
     @Override
@@ -68,4 +139,5 @@ public class Teken extends JFrame implements MouseListener {
     public void mouseExited(MouseEvent mouseEvent) {
 
     }
+
 }
